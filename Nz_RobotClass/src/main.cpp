7cf63f4e4c088a7270baeb0461d7554d202fb45e
Nicolass Zuo <nicolass5913@gmail.com>
2022-03-15 14:20:17 +0800

    sensor部分未定义完全

--- Nz_RobotClass/src/main.cpp
+++ Nz_RobotClass/src/main.cpp
@@ -5,61 +5,86 @@
 #define Motor1_Pin_0 5
 #define Motor1_Pin_1 6
 
-void dc_motor_drive(motor_number, direction, time)
-{
-    /******
-     * 直流电机驱动函数
-     * motor_number{0, 1}
-     * direction{front, back, right, left}
-     ******/
+#define Sensor0 
+#define Sensor1
+#define Sensor2
 
-    int pin0, pin1;
+/*----------------------------------------------------------------
+ * 直流电机驱动函数
+ * direction{front, back, right, left}
+ * time(ms)
+----------------------------------------------------------------*/
+void dc_motor_drive(direction, time) {
 
     // 根据原理图，
     // motor0位于Bigfish拓展板左侧，连接引脚为9、10
     // motor1位于Bigfish拓展板右侧，连接引脚为5、6
-    if (motor_number == 0)
-    {
-        pin0 = Motor0_Pin_0;
-        pin1 = Motor0_Pin_1;
-    }
-    else
-    {
-        pin0 = Motor1_Pin_0;
-        pin1 = Motor1_Pin_1;
-    }
 
-    switch (direction)
-    {
-    case front:
-    {
-        digitalWrite(pin0, HIGH);
-        digitalWrite(pin1, HIGH);
-        delay(time);
-    }
-    case back:
-    {
-        digitalWrite(pin0, LOW);
-        digitalWrite(pin1, LOW);
-        delay(time);
-    }
-    case right:
-    {
-        digitalWrite(pin0, HIGH);
-        digitalWrite(pin1, LOW);
-        delay(time);
-    }
-    case left:
-    {
-        digitalWrite(pin0, LOW);
-        digitalWrite(pin1, LOW);
-        delay(time);
-    }
+    switch (direction) {
+        // 向前
+        case front:{
+            digitalWrite(Motor0_Pin_0, HIGH);
+            digitalWrite(Motor0_Pin_1, HIGH);
+            digitalWrite(Motor1_Pin_0, HIGH);
+            digitalWrite(Motor1_Pin_1, HIGH);
+            delay(time);
+        }
+        // 向后
+        case back:{
+            digitalWrite(Motor0_Pin_0, LOW);
+            digitalWrite(Motor0_Pin_1, LOW);
+            digitalWrite(Motor1_Pin_0, LOW);
+            digitalWrite(Motor1_Pin_1, LOW);
+            delay(time);
+        }
+        // 向右
+        case right:{
+            digitalWrite(Motor0_Pin_0, LOW);
+            digitalWrite(Motor0_Pin_1, HIGH);
+            digitalWrite(Motor1_Pin_0, LOW);
+            digitalWrite(Motor1_Pin_1, HIGH);
+            delay(time);
+        }
+        //向左
+        case left:{
+            digitalWrite(Motor0_Pin_0, HIGH);
+            digitalWrite(Motor0_Pin_1, LOW);
+            digitalWrite(Motor1_Pin_0, HIGH);
+            digitalWrite(Motor1_Pin_1, LOW);
+            delay(time);
+        }
     }
 }
 
-void setup()
-{
+/*---------------------------------------------------------------
+ * 传感器数据追踪函数
+ * 默认使用3个传感器巡线，从左到右依次为0、1、2
+---------------------------------------------------------------*/
+void track() {
+    int data[3];
+
+    //读取三个传感器值
+    data[0] = digitalRead(sensor0);
+    data[1] = digitalRead(sensor1);
+    data[2] = digitalRead(sensor2);
+
+    //左右都没有检测到，直行
+    if(!data[0] && !data[1] && !data[2]) {
+        dc_motor_drive(front, 50);
+    }
+
+    //左边检测到黑线
+    if(data[0] || data[1]) {
+        dc_motor_drive(left, 50);
+    }
+    
+    //右边检测到黑线
+    if(data[1] || data[2]) {
+        dc_motor_drive(right, 50);
+    }
+}
+
+void setup() {
     // 直流电机输出引脚配置
     // Motor0:
     pinMode(Motor0_Pin_0, OUTPUT);
@@ -69,6 +94,6 @@ void setup()
     pinMode(Motor1_Pin_1, OUTPUT);
 }
 
-void loop()
-{
+void loop() {
+    track();
 }
